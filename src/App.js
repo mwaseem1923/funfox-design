8c005ac9b31d7ebea533df9e9e3511f3fa800d1f
developer_13 <developer@dhives.com>
2024-03-28 06:56:10 +0500

    first slide styling

--- src/App.js
+++ src/App.js
@@ -20,13 +20,17 @@ function App() {
     //   </header>
     // </div>
     <div className="story-writing-elements-container">
-    <h1 className="title">funfox</h1>
+          <img src='favicon.ico' className='logo' />
+          <img src='favicon.ico' className='' />
+
+    <div className="inner-container">
+
     <h2 className="subtitle">Week 1 Elements of Story Writing</h2>
-    <p className="text-content">
+    <p className="">
       Welcome to term 2 of Writers Club. Are you excited for this amazing
       journey?
     </p>
-    <p className="text-content">
+    <p className="">
       Over the next week, we will be practicing the different elements of
       story writing. Our aim is to make stories more interesting and
       exciting.
@@ -37,23 +41,33 @@ function App() {
       has the same key elements :
     </p>
     <ul className="key-elements-list">
-      <li>SETTING</li>
-      <li>CHARACTERS</li>
-      <li>PLOT</li>
-      <li>PROBLEM OR CONFLICT</li>
-      <li>RESOLUTION</li>
+      <li className='setting'>SETTING</li>
+      <li className='setting'>CHARACTERS</li>
+      <li className='setting'>PLOT</li>
+      <li className='setting'>PROBLEM OR CONFLICT</li>
+      <li className='setting'>RESOLUTION</li>
     </ul>
-    <p className="text-content">
+    <p className="">
       You must be familiar with some of these. If not, do not worry! We will
       cover all these elements as we go along.
     </p>
-    <p className="text-content">
+    <p className="last">
       For today's lesson, we will try to understand and practice writing the
-      setting for our stories.
-    </p>
-    <p className="text-content last-text-content">
+      setting for our stories.<br />
       The setting is an important element of every fiction story.
     </p>
+    </div>
+    <div className='flex-controlls'>
+      <img src='favicon.ico' />
+      <div>
+      <img src='favicon.ico' />
+      dropdown
+      <img src='favicon.ico' />
+
+      </div>
+      <img src='favicon.ico' />
+
+    </div>
   </div>
   );
 }
